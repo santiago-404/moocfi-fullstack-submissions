cd36313229c603adb309499ab138dd81e674a4dd
abraham <asantiago.9861@umak.edu.ph>
2026-02-07 21:44:30 +0800

    copied the phonebook frontend into the fullstack dir

---
+++ part3/phonebook-fullstack/phonebook-frontend/src/components/PersonsList.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react/jsx-runtime";
+
+const Persons = ({list, handleDelete}) => {
+  return (
+    <div>
+      {list.map(element => 
+        <Person key={element.id} props={element} handleDelete={handleDelete} id={element.id}/>)}
+    </div>
+  )
+}
+
+const Person = ({props: {name, number}, id, handleDelete}) => {  
+  return (
+    <Fragment>
+      <li>
+        {name} | {number} 
+        <button onClick={handleDelete(id)}>delete</button> 
+      </li>
+    </Fragment>
+  )
+}
+
+export default Persons;
